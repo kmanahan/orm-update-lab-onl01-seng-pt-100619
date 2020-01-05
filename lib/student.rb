1d09f19459ba7af5aaf029ae897b72c9c39e9fa2
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-05 18:41:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ attr_reader :id
     VALUES (?,?) 
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute(SELECT)
+    @id = DB[:conn].execute(SELECT last_row)
   end 
 
 end
